251246cd9a0fc9424971e79fcf7bbb9762b7a2de
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2017-07-18 19:42:59 +0200

    Adjust tests.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,9 @@
 #include <iostream>
-
 #include "timer.hpp"
 
 int main() {
-  Timer t;
-  t.call([](){std::cout << "Hello world!\n";}, 10, 3);
-  t.call([](){std::cout << "Hello other world!\n";}, 1, 1);
+  Timer timer([](){std::cout << "Hello from timer!\n";}, 2, 3);
   std::this_thread::sleep_for(std::chrono::seconds(5));
   std::cout << "Hello from main.\n";
-  t.run = false;
   return 0;
 }
--- timer.cpp
+++ timer.cpp
@@ -3,14 +3,14 @@
 Timer::Timer(const std::function<void()> &func,
              const int delay,
              const int runs) : run(true),
-                               future(std::async(std::launch::async, [&](const int delay, const int runs) {}, delay, runs)){
-  for (int i = 0; i < runs; i++) {
-    if (run) {
-      std::this_thread::sleep_for(std::chrono::seconds(delay));
-      func();
-    }
-  }
-}
+                               future(std::async(std::launch::async, [&](const int delay, const int runs) {
+                                 for (int i = 0; i < runs; i++) {
+                                   if (run) {
+                                     std::this_thread::sleep_for(std::chrono::seconds(delay));
+                                     func();
+                                   }
+                                 }
+                               }, delay, runs)) {}
 
 void Timer::stop() {
   run = false;
